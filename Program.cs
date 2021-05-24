e3aeec2d795c11ca2a676972f1eb36e7eae747e7
Vere Miller <72162006+vmille25@users.noreply.github.com>
2021-05-24 16:07:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+namespace HTTPClient_APP
+{
+    class Program
+    {
+        public static string fileOutput = @"C:\Users\vdmil\source\repos\HTTPClient_APP\HTTPClient_APP\output.txt";
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the URL that you want an html response from: ");
+            string url = Console.ReadLine();
+            var awaiter = CallURL(url);
+            if (awaiter.Result != "")
+            {
+                File.WriteAllText(fileOutput, awaiter.Result);
+                Console.WriteLine("HTML Response output to " + fileOutput);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        public static async Task<string> CallURL(string url)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(url);
+            return await response;
+        }
+    }
+}
